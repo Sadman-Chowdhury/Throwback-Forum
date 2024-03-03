6b10105a693f29c13edb08b9657c8ba8b8b3e4f0
Sadman-Chowdhury <sadman.ch49@gmail.com>
2024-03-03 20:47:34 +0600

    adding highlight after clicking post button, searching option, loading spinner added

--- js/forum.js
+++ js/forum.js
@@ -1,36 +1,24 @@
-const loadData = async() => {
-    const res = await fetch('https://openapi.programming-hero.com/api/retro-forum/posts')
+const loadData = async(searchText) => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/retro-forum/posts?category=${searchText}`)
+    console.log(res)
     const data = await res.json()
     console.log(data)
-    const posts = data.posts
-    displayData(posts);
+    const postData = data.posts
+    displayData(postData);
 }
 
 const displayData = (posts) => {
 //1. get the container
 const postContainer = document.getElementById('post-container')
 
-// //Clear phone container card before
-// phoneContainer.textContent = '';
-
-// //Display show all button if there are more than 12 phones
-// const showAllContainer = document.getElementById('show-all-container');
-// if(phones.length > 12 && !isShowAll){
-//     showAllContainer.classList.remove('hidden');
-// }else{
-//     showAllContainer.classList.add('hidden');
-// }
-
-//Display only first 12 phones if not show all
-// if(!isShowAll){
-//     phones = phones.slice(0,12);
-// }
+postContainer.textContent = '';
 
 posts.forEach(post => {
     //Creating a div
     const postCard = document.createElement('div');
     postCard.classList = `bg-[#F3F3F5] rounded-3xl p-10 flex flex-col gap-4`
 
+    const highlightContainer = document.getElementById('highlight-container')
     //active color condition
     const activeColor = post.isActive ? 'bg-green-500' : 'bg-red-500';
 
@@ -67,19 +55,77 @@ posts.forEach(post => {
                     </div>
                 </div>
                 <div class=" flex items-center justify-center">
+                    <button class="highlight-btn">
                     <i
-                        class="fa-regular fa-envelope bg-[#10B981] text-white p-1 rounded-[50px]"></i>
+                    class="fa-regular fa-envelope bg-[#10B981] text-white p-1 rounded-[50px]"></i></button>
                 </div>
             </div>
         </div>
     </div>
 </div>
     `;
-    //4. AppendChild
+    //AppendChild
     postContainer.appendChild(postCard);
+
+    toggleLoadingSpinner(false);
+
+    // Add event listener to the highlight-btn inside the post card
+    const highlightBtn = postCard.querySelector('.highlight-btn');
+    highlightBtn.addEventListener('click', () => {
+        // Create a new div for the post details
+        const postDetails = document.createElement('div');
+        postDetails.classList = `flex justify-between bg-white p-4 rounded-2xl gap-4 mb-4`;
+        // Set the content for the post details
+        postDetails.innerHTML = `
+        <h3 class="w-1/2">${post.title}</h3>
+            <div class="flex items-center gap-3">
+                 <i class="fa-regular fa-eye"></i>
+                 <span>${post.view_count}</span>
+             </div>
+        `;
+        // Append the post details to the details container
+        highlightContainer.appendChild(postDetails);
+
+        const currentCount = getTextElementValueById('post-count')
+        const updatedCount = currentCount + 1;
+        setTextElementValueById('post-count', updatedCount);
+
+    });
 });
-// //Hide loading spinner
-// toggleLoadingSpinner(false);
+
 }
 
-loadData();
+const getTextElementValueById = (elementId) => {
+    const element = document.getElementById(elementId);
+    const elementValueText = element.innerText;
+    const value = parseInt(elementValueText);
+    return value;
+}
+
+const setTextElementValueById = (elementId, value) => {
+    const element = document.getElementById(elementId);
+    element.innerText = value;
+}
+
+//Handle search button
+const handleSearch = () => {
+    toggleLoadingSpinner(true);
+
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    console.log(searchText);
+
+        setTimeout(() => {
+            loadData(searchText);
+        }, 2000);
+}
+
+//toggle loading spinner
+const toggleLoadingSpinner = (isLoading, delay) => {
+    const loadingSpinner = document.getElementById('loading-spinner');
+    if(isLoading){
+            loadingSpinner.classList.remove('hidden');
+    }else{
+        loadingSpinner.classList.add('hidden')
+    }
+}
